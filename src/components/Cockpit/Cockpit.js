6f3cb1211fa9feaa4e0bb52232db925ca9cc1b01
System Administrator <root@MacBook-Pro-Baryskhan.local>
2018-12-01 20:46:24 +0600

    React.memo() for re-rendering functional component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,33 +1,32 @@
 import React from 'react';
+
 import classes from './Cockpit.css';
 import Aux from '../../hoc/Aux';
 
-const cockpit = (props) => {
+const cockpit = ( props ) => {
+    const assignedClasses = [];
+    let btnClass = classes.Button;
+    if ( props.showPersons ) {
+        btnClass = [classes.Button, classes.Red].join( ' ' );
+    }
 
-  const assignedClasses = [];
-  let btnClass = '';
+    if ( props.persons.length <= 2 ) {
+        assignedClasses.push( classes.red ); // classes = ['red']
+    }
+    if ( props.persons.length <= 1 ) {
+        assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    }
 
-  if(props.shwoPersons){
-    btnClass = classes.Red;
-  }
-
-  if ( props.persons.length <= 2 ) {
-    assignedClasses.push( classes.red ); // classes = ['red']
-  }
-  if ( props.persons.length <= 1 ) {
-    assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
-  }
-
-  return(
-    <Aux>
-      <h1>Hi, I'm a React App</h1>
-      <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-      <button
-        className={btnClass}
-        onClick={props.clicked}>Toggle Persons</button>
-    </Aux>
-  );
+    return (
+        <Aux>
+            <h1>{props.appTitle}</h1>
+            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+            <button onClick={props.login}>Log in</button>
+        </Aux>
+    );
 };
-
 
 export default cockpit;
--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,24 +1,28 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 
 // const withClass = (WrappedComponent, className) => {
-//   return (props) => (
-//     <div className={className}>
-//       <WrappedComponent {...props}/>
-//     </div>
-//   );
-// };
-
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     )
+// }
 
 const withClass = (WrappedComponent, className) => {
-  return class extends Component{
-    render(){
-      return (
-        <div className={className}>
-          <WrappedComponent {...this.props}/>
-        </div>
-      )
+    const WithClass = class extends Component {
+        render () {
+            return (
+                <div className={className}>
+                    <WrappedComponent ref={this.props.forwardedRef} {...this.props} />
+                </div>
+            )
+        }
     }
-  }
+
+    return React.forwardRef((props, ref) => {
+        return <WithClass {...props} forwardedRef={ref} />
+    });
 }
+
 
 export default withClass;
